e7c6f7135752cb99b8842de36c3293b721dff88d
Karey Higuera <karey.higuera@gmail.com>
2020-01-20 09:58:49 -0600

    Add labels to form page

--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -15,10 +15,12 @@ import female from '../images/female.svg'
 
 const BlessingForm = () => {
   const today = new Date() // default value for the calendar
+  const brandColor = getComputedStyle(document.documentElement).getPropertyValue('--brand-color');
+  const accentColor = getComputedStyle(document.documentElement).getPropertyValue('--accent-color');
   return (
     <Layout>
       <SEO title="Form" />
-      <h2 style={{textAlign: 'center'}}>Enter your patriarchal blessing information below.</h2>
+      <h2 style={{ textAlign: 'center' }}>Enter your patriarchal blessing information below.</h2>
       <Formik
         initialValues={{
           firstName: '', middleName: '', lastName: '',
@@ -43,16 +45,19 @@ const BlessingForm = () => {
         {({ isSubmitting, setFieldValue }) => (
           <Form className="blessing-form shadow">
 
+            <h5>First Name</h5>
             <Field type="text" name="firstName" placeholder="First Name" />
             <ErrorMessage name="firstName" component="div" className="error-box" />
 
+            <h5>Middle Name <span className="optional-tag">(optional)</span></h5>
             <Field type="text" name="middleName" placeholder="Middle Name" />
 
+            <h5>Last Name</h5>
             <Field type="text" name="lastName" placeholder="Last Name" />
             <ErrorMessage name="lastName" component="div" className="error-box" />
 
+            <h5>Gender</h5>
             <div className="gender-toggle">
-              <label htmlFor="gender">Gender</label>
               <Field name="gender">
                 {({ field: { value, onChange } }) => (
                   <>
@@ -65,22 +70,32 @@ const BlessingForm = () => {
                         unchecked: <img alt="" src={female} />
                       }}
                     />
-                    <span className="gender-label">{value ? 'Male' : 'Female'}</span>
+                    <h4
+                      className="gender-label"
+                      style={{ color: value ? brandColor : accentColor }}
+                    >
+                      {value ? 'Male' : 'Female'}
+                    </h4>
                   </>
                 )}
               </Field>
             </div>
 
+            <h5>Mother's Full Name <span className="optional-tag">(optional)</span></h5>
             <Field type="text" name="mother" placeholder="Mother's Full Name" />
 
+            <h5>Father's Full Name <span className="optional-tag">(optional)</span></h5>
             <Field type="text" name="father" placeholder="Father's Full Name" />
 
+            <h5>Patriarch's Full Name</h5>
             <Field type="text" name="patriarch" placeholder="Patriarch's Full Name" />
             <ErrorMessage name="patriarch" component="div" className="error-box" />
 
+            <h5>Stake or District</h5>
             <Field type="text" name="stake" placeholder="Stake or District" />
             <ErrorMessage name="stake" component="div" className="error-box" />
 
+            <h5>Date of the Blessing</h5>
             <Field name="date">
               {({ field: { value } }) => (
                 <Calendar
@@ -92,7 +107,8 @@ const BlessingForm = () => {
               )}
             </Field>
 
-            <Field as="textarea" name="blessing" placeholder="Patriarchal Blessing" style={{resize: 'vertical'}} />
+            <h5>Blessing Text</h5>
+            <Field as="textarea" name="blessing" placeholder="Patriarchal Blessing" style={{ resize: 'vertical' }} />
             <ErrorMessage name="blessing" component="div" className="error-box" />
 
             <button className="action-button" type="submit" disabled={isSubmitting}>Generate Document</button>
