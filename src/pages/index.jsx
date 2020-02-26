699972247559ff67aeddec6d001f10ec7cc2d4e9
Karey Higuera <karey.higuera@gmail.com>
2020-02-25 19:43:31 -0600

    Remove extra animate presence from index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,34 +4,32 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import DocPreview from '../components/doc-preview'
-import {motion, AnimatePresence} from 'framer-motion'
+import { motion} from 'framer-motion'
 
 import '../components/css/index.css'
 
 const IndexPage = () => {
   return (
     <Layout>
-      <AnimatePresence exitBeforeEnter>
-        <SEO title="Home" />
-        <motion.div className="home" key="home"
-          initial="hidden"
-          animate="visible"
-          exit={{y: 100, opacity: 0}}
-          variants={{
-            hidden: {y: 100, opacity: 0},
-            visible: {y: 0, opacity: 1}
-          }}
-          transition={{ duration: .75 }}
-        >
-          <div className="welcome">
-            <h1>Welcome to Scriptly!</h1>
-            <p>Your patriarchal blessing is like a personal chapter of scripture written <span style={{ fontStyle: 'italic' }}>just for you</span>.</p>
-            <p>We think it should look like one, too.</p>
-          </div>
-          <DocPreview />
-        </motion.div>
-        <Link to="/blessing" className="action-button">Get started</Link>
-      </AnimatePresence>
+      <SEO title="Home" />
+      <motion.div className="home" key="home"
+        initial="hidden"
+        animate="visible"
+        exit={{ y: 100, opacity: 0 }}
+        variants={{
+          hidden: { y: 100, opacity: 0 },
+          visible: { y: 0, opacity: 1 }
+        }}
+        transition={{ duration: .75 }}
+      >
+        <div className="welcome">
+          <h1>Welcome to Scriptly!</h1>
+          <p>Your patriarchal blessing is like a personal chapter of scripture written <span style={{ fontStyle: 'italic' }}>just for you</span>.</p>
+          <p>We think it should look like one, too.</p>
+        </div>
+        <DocPreview />
+      </motion.div>
+      <Link to="/blessing" className="action-button">Get started</Link>
     </Layout>
   )
 }
