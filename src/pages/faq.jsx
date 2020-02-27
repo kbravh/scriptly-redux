4626c219ef398676c0fca96c9c0859dfd0c99f34
Karey Higuera <karey.higuera@gmail.com>
2020-02-26 20:48:28 -0600

    add FAQ page

---
+++ src/pages/faq.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../components/css/faq.css'
+
+const FAQ = () => (
+  <>
+    <h1>Frequently Asked Questions</h1>
+    <ul className="faq-list">
+      <li>
+        <h3>Is my patriarchal blessing going to be seen by anyone else?</h3>
+        <p>This is a completely automated process, so no one sees your patriarchal blessing at any point. Your documents will automatically be erased after 24 hours for more security.</p>
+      </li>
+    </ul>
+  </>
+)
+
+export default FAQ
