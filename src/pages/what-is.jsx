8993116bda2b7297e305a5a8397f5b8f1ed1a3a2
Karey Higuera <karey.higuera@gmail.com>
2020-06-14 12:21:56 -0500

    move to linux, add what-is page

---
+++ src/pages/what-is.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {useIntl} from 'gatsby-plugin-intl'
+import SEO from "../components/seo"
+
+const whatIs = () => {
+    return (
+        <>
+            <SEO title="What is a patriarchal blessing?" />
+            <h1 className="faq-header">What is a patriarchal blessing?</h1>
+            <p>As members of the Church of Jesus Christ of Latter-day Saints, we believe that God lives, cares for us, and guides us in our daily lives. One of the most special gifts that he gives us is something called a patriarchal blessing.</p>
+            <p>A member of the church who holds the office of Patriarch will lay his hands on our head and give us a special blessing. This blessing contains divine guidance from the Lord to help us make decisions in our lives. It's what the Lord would tell us himself if he were there to give us the blessing.</p>
+        </>
+    )
+
+}
+
+export default whatIs
