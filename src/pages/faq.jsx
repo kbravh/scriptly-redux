2a2b915b198c07d76a73c2bed3dc3f89fae5f30a
Karey Higuera <karey.higuera@gmail.com>
2020-06-27 22:00:12 -0500

    add what is page

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useIntl} from 'gatsby-plugin-intl'
+import { useIntl } from 'gatsby-plugin-intl'
 import SEO from "../components/seo"
 
 import '../components/css/faq.css'
@@ -7,19 +7,20 @@ import '../components/css/faq.css'
 const FAQ = () => {
   const intl = useIntl()
   return (
-  <>
-    <SEO title="FAQs" />
-    <h1 className="faq-header">{intl.formatMessage({id: "faq.header"})}</h1>
-    <ul className="faq-list">
-      <li>
-        <h3>{intl.formatMessage({id: "faq.seen-by-others"})}</h3>
-        <p>{intl.formatMessage({id: "faq.seen-by-others-answer"})}</p>
-      </li>
-    </ul>
-    <div className="contact-me">
-    {intl.formatMessage({id: "faq.contact"})} <a href="mailto:info@laborforzion.com">info@laborforzion.com</a>
-    </div>
-  </>
-)}
+    <>
+      <SEO title="FAQs" />
+      <h1 className="faq-header">{intl.formatMessage({ id: "faq.title" })}</h1>
+      <ul className="faq-list">
+        <li>
+          <h3>{intl.formatMessage({ id: "faq.seen-by-others" })}</h3>
+          <p>{intl.formatMessage({ id: "faq.seen-by-others-answer" })}</p>
+        </li>
+      </ul>
+      <div className="contact-me">
+        {intl.formatMessage({ id: "faq.contact" })} <a href="mailto:info@laborforzion.com">info@laborforzion.com</a>
+      </div>
+    </>
+  )
+}
 
 export default FAQ
