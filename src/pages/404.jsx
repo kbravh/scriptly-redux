abc8a3ab731e7c7e91c009431adceb264c92a23b
Karey Higuera <karey.higuera@gmail.com>
2020-06-29 14:50:25 -0500

    animate 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,15 +1,34 @@
 import React from "react"
-import {useIntl} from 'gatsby-plugin-intl'
+import { useIntl } from 'gatsby-plugin-intl'
 import SEO from "../components/seo"
+import { motion } from 'framer-motion'
 
 const NotFoundPage = () => {
   const intl = useIntl()
   return (
-  <>
-    <SEO title={intl.formatMessage({id: "404.title"})} />
-    <h1>{intl.formatMessage({id: "404.notfound"})}</h1>
-    <p>{intl.formatMessage({id: "404.message"})}</p>
-  </>
-)}
+    <>
+      <SEO title={intl.formatMessage({ id: "404.title" })} />
+      <motion.div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyItems: "center",
+          alignItems: "center"
+      }}
+        initial="hidden"
+        animate="visible"
+        exit={{ y: 100, opacity: 0 }}
+        variants={{
+          hidden: { y: 100, opacity: 0 },
+          visible: { y: 0, opacity: 1 }
+        }}
+        transition={{ duration: .75 }}
+      >
+        <h1>{intl.formatMessage({ id: "404.notfound" })}</h1>
+        <p>{intl.formatMessage({ id: "404.message" })}</p>
+      </motion.div>
+    </>
+  )
+}
 
 export default NotFoundPage
